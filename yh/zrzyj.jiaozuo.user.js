957ae259e5bcb88585f5116fc7bea5824e5fe5b7
ef6 <evan7fan@outlook.com>
2022-02-28 16:19:49 +0800

    use EventListener

--- yh/zrzyj.jiaozuo.user.js
+++ yh/zrzyj.jiaozuo.user.js
@@ -6,7 +6,7 @@
 // @license      GPL3.0
 // @date         2022.01.18
 // @modified     2022.02.10
-// @version      1.1.0
+// @version      1.2.0
 // @icon         http://zrzyj.jiaozuo.gov.cn/favicon.ico
 // @match        http*://jy.zrzyj.jiaozuo.gov.cn*/trade-engine/trade/resources*
 // @match        http*://jy.zrzyj.jiaozuo.gov.cn*/trade-engine/trade/collectiveresources*
@@ -16,27 +16,22 @@
 (function() {
     'use strict';
     function displayMu(){
-        Array.from(document.querySelectorAll(".resource-list")).forEach((Elem) => {
-            if(! Elem.querySelector(".hasMu")){
-                let mj=Elem.querySelector("span[ng-bind*=CRMJ]").innerText;
-                let jg=Elem.querySelector("span[ng-bind*=ZGBJ]").innerText.slice(1);
-                if(mj>0 && jg>0){
-                    let dj=(jg*10000)/mj;
-                    let djm=dj/15;
-                    let el = document.createElement("a");
-                    el.className="hasMu color-red font-bold";
-                    el.innerText=String.format("{0}元/㎡ {1}万元/亩",dj.toFixed(0),djm.toFixed(0));
-                    Elem.querySelector(".resource-list-header").append(el)
-                }
+        console.log("DOMNodeInserted");
+        Array.from(document.querySelectorAll(".resource-list"))
+          .forEach((Elem) => {
+            if(! Elem.querySelector(".hasMu") && Elem.querySelector("span[ng-bind*=CRMJ]") && Elem.querySelector("span[ng-bind*=ZGBJ]")){
+            let mj=Elem.querySelector("span[ng-bind*=CRMJ]").innerText;
+            let jg=Elem.querySelector("span[ng-bind*=ZGBJ]").innerText.slice(1);
+            if(mj>0 && jg>0){
+                let dj=(jg*10000)/mj;
+                let djm=dj/15;
+                let el = document.createElement('a');
+                el.className="hasMu font-bold";
+                el.innerHTML=String.format("<i>{0} 元/㎡</i><a>&nbsp;</a><i>{1} 万元/亩</i>",dj.toFixed(0),djm.toFixed(1));
+                Elem.querySelector(".resource-list-header").append(el)
+            }
             }
         });
     }
-    new MutationObserver((mutationRecords, observer) => {
-        console.log("document is change");
-        setTimeout(displayMu,1000);
-        //observer.disconnect();
-    }).observe(document.querySelector(".land-list-container"), {
-        childList: true,
-        //subtree: true
-    });
+    document.querySelector(".land-list-container").addEventListener("DOMNodeInserted", displayMu);
 })();
