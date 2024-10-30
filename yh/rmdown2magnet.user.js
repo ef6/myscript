278ae80130664ca1aec3f473118796fbd650eb8c
Evan <evan7fan@outlook.com>
2024-10-30 17:23:06 +0800

    2024-10-30

--- yh/rmdown2magnet.user.js
+++ yh/rmdown2magnet.user.js
@@ -5,24 +5,25 @@
 // @namespace    https://ef6.github.io
 // @license      GPL3.0
 // @date         2022.01.18
-// @modified     2023.04.13
-// @version      1.0.7
+// @modified     2024.10.30
+// @version      1.0.8
 // @updateURL    https://cdn.jsdelivr.net/gh/ef6/myscript@main/yh/rmdown2magnet.user.js
 // @icon         https://rmdown.com/favicon.ico
-// @include      http*://*.com/htm_data/*.html
-// @include      http*://*.com/htm_mob/*.html
+// @include      http*://*/htm_data/*.html
+// @include      http*://*/htm_mob/*.html
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
 (function() {
-	'use strict';
-	let node = document.querySelectorAll(".tpc_cont a[href*=rmdown][href*=hash]");
-	for(let n of node){
-		let magnet = 'magnet:?xt=urn:btih:' + n.href.split('=')[1].slice(3);
-		console.log(magnet);
-		n.innerText=magnet;
-		n.href=magnet;
-		n.className="h f18";
-	}
+    'use strict';
+    let title = document.querySelector('#main>.f18').innerText.slice(0,30);
+    let node = document.querySelectorAll(".tpc_cont a[href*=rmdown][href*=hash]");
+    for(let n of node){
+        let magnet = 'magnet:?xt=urn:btih:' + n.href.split('=')[1].slice(3);
+        console.log(title,magnet);
+        n.innerText=magnet;
+        n.href=magnet + '&dn=' + title;
+        n.className="h f18";
+    }
 })();
